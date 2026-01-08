2ec9e8f2c670c55306a4cca622506bbe2e616bce
mithlesh-vishwakarma <mv9898733607@gmail.com>
2026-01-09 00:00:10 +0530

    all fixed

--- src/pages/ProductCollections/Granites.tsx
+++ src/pages/ProductCollections/Granites.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import { useMemo } from 'react';
 import ProductShowcase from '../../components/ProductShowcase';
 
 // Placeholder data - Replace with actual Granite images
--- src/pages/ProductCollections/OtherMaterials.tsx
+++ src/pages/ProductCollections/OtherMaterials.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import { useMemo } from 'react';
 import ProductShowcase from '../../components/ProductShowcase';
 
 const productData = {
